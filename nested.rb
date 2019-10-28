526f568292bd9042597c38b2ea99d9268339fd27
Tanesha Goodman <t24good@icloud.com>
2019-10-28 13:23:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,7 +122,7 @@ def adding_to_dennis
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+        programmer_hash[:languages] = "Assembly"
           
         }
         
