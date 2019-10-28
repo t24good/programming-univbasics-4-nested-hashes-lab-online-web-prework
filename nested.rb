13d7916d5a51282be42b839a6d98a77e3b54bf3d
Tanesha Goodman <t24good@icloud.com>
2019-10-28 13:27:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,9 +38,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -54,18 +57,19 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+ 		  }
+ 		programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
-	programmer_hash = 
- 		{
+  programmer_hash = 
+ 		{  
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,8 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash
 end
 
 def changing_alan
@@ -91,7 +94,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -99,9 +102,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable
+    
+    programmer_hash
      
 end
 
@@ -120,6 +123,8 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-
+ 		}    
+        programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+        
+  programmer_hash
 end
